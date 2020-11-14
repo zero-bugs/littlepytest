74dd051ac6e47d569c07dc5c4d62e7180dca11f6
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2020-11-14 21:27:30 +0800

    Create pic_attr.py

---
+++ models/pic_attr.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class PicAttr:
+    '''image attribute'''
+
+    def __init__(self):
+        self.id = ''
+        self.width = 0
+        self.height = 0
+        self.file_size = 0
+        self.score = ''
+        self.md5 = ''
+        self.previewUrl = ''
+        self.fileUrl = ''
+        self.tags = ''
+        self.create_at = 0
+        self.creator_id = ''
+        self.source = ''

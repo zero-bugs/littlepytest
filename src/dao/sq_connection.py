6303886d732677ad5ea9eb38f7e2f187f91bfca1
zero-bugs <38752615+zero-bugs@users.noreply.github.com>
2021-05-22 16:27:24 +0800

    Update sq_connection.py

--- src/dao/sq_connection.py
+++ src/dao/sq_connection.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-
+import os
 import sqlite3
 import threading
 
 from src.config.common_config import CommonConstant
 from src.config.lick_config import LickConfig
+from src.logs.log_utils import LogUtils
 from src.models.BaseImgMeta import BaseImgMeta
+
+
+def mapImgToList(imgs: BaseImgMeta):
+    if imgs is None:
+        return list()
+
+    result = list()
+    for img in imgs:
+        cLst = list()
+        cLst.append(img.img_id),
+        cLst.append(img.width),
+        cLst.append(img.height),
+        cLst.append(img.file_size),
+        cLst.append(img.file_url),
+        cLst.append(img.file_ext),
+        cLst.append(img.tags),
+        cLst.append(img.md5),
+        cLst.append(img.score),
+        cLst.append(img.create_at),
+        cLst.append(img.author),
+        cLst.append(img.creator_id),
+        cLst.append(img.img_source),
+        result.append(cLst)
+    else:
+        return result
 
 
 class SqliteManager:
@@ -14,8 +40,9 @@ class SqliteManager:
 
     def __init__(self, *args, **kwargs):
         """链接数据库，若数据库不存在则创建"""
-        self.conn = sqlite3.connect("%s" % CommonConstant.basicConfig[LickConfig.lickType]["dbLibPath"],
-                                    check_same_thread=False)
+        dbPath = CommonConstant.basicConfig[LickConfig.lickType]["dbLibPath"]
+        LogUtils.log(f"db path:{dbPath}, current path:{os.path.abspath('.')}")
+        self.conn = sqlite3.connect(dbPath, check_same_thread=False)
         # 在内存中创建数据库
         # conn = sqlite3.connect(":memory:")
         # 创建游标对象
@@ -24,7 +51,7 @@ class SqliteManager:
         # 创建数据表
         sql = (
             "create table if not exists "
-            "tbl_img(img_id integer primary key not null,"
+            "tbl_img(img_id VARCHAR(20) primary key not null ,"
             "width integer,"
             "height integer,"
             "file_size integer,"
@@ -33,7 +60,7 @@ class SqliteManager:
             "tags text,"
             "md5 text,"
             "score integer,"
-            "create_at text,"
+            "create_at datetime,"
             "author text,"
             "creator_id text,"
             "img_source text)"
@@ -48,9 +75,9 @@ class SqliteManager:
                     SqliteManager._instance = SqliteManager(*args, **kwargs)
         return SqliteManager._instance
 
-    def insertImg(self, pic: BaseImgMeta):
+    def insertImg(self, img: BaseImgMeta):
         """插入图片"""
-        if pic is None:
+        if img is None:
             return False
 
         try:
@@ -58,65 +85,50 @@ class SqliteManager:
             self.cur.execute(
                 "insert or ignore into tbl_img "
                 "(img_id,width,height,file_size,file_url,file_ext,tags,md5,score,create_at,author,creator_id,img_source) "
-                "values (?,?,?,?,?,?,?,?,?,?,?,?)",
+                "values (?,?,?,?,?,?,?,?,?,?,?,?,?)",
                 (
-                    pic.img_id,
-                    pic.width,
-                    pic.height,
-                    pic.file_size,
-                    pic.file_url,
-                    pic.file_ext,
-                    pic.tags,
-                    pic.md5,
-                    pic.score,
-                    pic.create_at,
-                    pic.author,
-                    pic.creator_id,
-                    pic.img_source
+                    img.img_id,
+                    img.width,
+                    img.height,
+                    img.file_size,
+                    img.file_url,
+                    img.file_ext,
+                    img.tags,
+                    img.md5,
+                    img.score,
+                    img.create_at,
+                    img.author,
+                    img.creator_id,
+                    img.img_source,
                 ),
             )
             self.conn.commit()
             return True
         except Exception as err:
-            print(err)
+            LogUtils.log(err)
             self.conn.rollback()
             return False
         finally:
             lock.release()
 
-    def batchInsertImg(self, pic: list):
+    def batchInsertImg(self, imgs: list):
         """批量插入图片"""
-        if pic is None:
+        if imgs is None:
             return False
-        elif len(pic) == 0:
+        elif len(imgs) == 0:
             return False
         try:
-            for p in pic:
-                lock.acquire(True)
-                self.cur.execute(
-                    "insert or ignore into tbl_img "
-                    "(img_id,width,height,file_size,file_url,file_ext,tags,md5,score,create_at,author,creator_id,img_source) "
-                    "values (?,?,?,?,?,?,?,?,?,?,?,?)",
-                    (
-                        pic.img_id,
-                        pic.width,
-                        pic.height,
-                        pic.file_size,
-                        pic.file_url,
-                        pic.file_ext,
-                        pic.tags,
-                        pic.md5,
-                        pic.score,
-                        pic.create_at,
-                        pic.author,
-                        pic.creator_id,
-                        pic.img_source
-                    ),
-                )
+            lock.acquire(True)
+            self.cur.executemany(
+                "insert or ignore into tbl_img "
+                "(img_id,width,height,file_size,file_url,file_ext,tags,md5,score,create_at,author,creator_id,img_source) "
+                "values (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                mapImgToList(imgs),
+            )
             self.conn.commit()
             return True
         except Exception as err:
-            print(err)
+            LogUtils.log(err)
             self.conn.rollback()
             return False
         finally:
@@ -129,7 +141,13 @@ class SqliteManager:
         result = []
         try:
             lock.acquire(True)
-            self.cur.execute("select * from tbl_img limit ? offset ? ", (limit, offset,))
+            self.cur.execute(
+                "select * from tbl_img limit ? offset ? ",
+                (
+                    limit,
+                    offset,
+                ),
+            )
             values = self.cur.fetchall()
             if values is None:
                 return None
